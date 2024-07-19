01289f401f9dc0af971cdf7c33e9c7eddc3709e5
Rombt <rombtnet@gmail.com>
2024-07-19 11:44:05 +0300

    mobile version of testimonials-block  is ready

--- app/src/assets/js/modules/sliders.js
+++ app/src/assets/js/modules/sliders.js
@@ -3,14 +3,22 @@ if (document.querySelector('.rmbt-testimonials-swiper')) {
     direction: 'horizontal',
     loop: true,
 
-    slidesPerView: 4,
-    spaceBetween: 40,
     centeredSlides: true,
     centerInsufficientSlides: true,
+
+    slidesPerView: 2,
+    spaceBetween: 10,
 
     navigation: {
       nextEl: '.button-next',
       prevEl: '.button-prev',
+    },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
     },
 
     // speed: 1400,
@@ -25,14 +33,21 @@ if (document.querySelector('.rmbt-testimonials-video-swiper')) {
     direction: 'horizontal',
     loop: true,
 
-    slidesPerView: 4,
-    spaceBetween: 40,
+    slidesPerView: 2,
+    spaceBetween: 10,
     centeredSlides: true,
     centerInsufficientSlides: true,
 
     navigation: {
       nextEl: '.button-next-video',
       prevEl: '.button-prev-video',
+    },
+
+    breakpoints: {
+      480: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
     },
 
     // speed: 1400,
