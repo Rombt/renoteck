dc361c045977d0e6e937b59c77c645d9e8f01a8c
RomRon <romron@ukr.net>
2024-07-27 07:17:18 +0300

    .

---
+++ app/src/ commercial_roofing.php
@@ -0,0 +1,25 @@
+<?php
+/* Template Name:  Commercial Roofing */
+?>
+
+
+<?php get_header(); ?>
+
+
+
+<main class="rmbt-financing-page">
+   <?php
+
+
+   // get_template_part('template-parts/parts/financing_block');
+   // get_template_part('template-parts/parts/benefits_block');
+   // get_template_part('template-parts/parts/finansing_about');
+   // get_template_part('template-parts/parts/banner_capture_form_block');
+   // get_template_part('template-parts/parts/testimonials_block');
+   // get_template_part('template-parts/parts/call_to_action_block');
+
+   ?>
+</main>
+
+
+<?php get_footer();
